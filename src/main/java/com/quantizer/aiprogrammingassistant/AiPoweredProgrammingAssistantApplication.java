afd11da7309eb75c586ea8d1ee6851827d05e24f
Quantiser <nitgvikas@gmail.com>
2023-04-08 14:23:28 +0530

    initial commit

---
+++ src/main/java/com/quantizer/aiprogrammingassistant/AiPoweredProgrammingAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.quantizer.aiprogrammingassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredProgrammingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPoweredProgrammingAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quantizer/aiprogrammingassistant/AiPoweredProgrammingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantizer.aiprogrammingassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPoweredProgrammingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
